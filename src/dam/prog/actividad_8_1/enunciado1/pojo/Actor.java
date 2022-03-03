08ff09e447255979fac23a52ddee7fd8b4b1141f
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-03 09:56:48 +0100

    src/dam/prog/actividad_8_1/enunciado1/pojo/Actor.java added

---
+++ src/dam/prog/actividad_8_1/enunciado1/pojo/Actor.java
@@ -0,0 +1,11 @@
+package dam.prog.actividad_8_1.enunciado1.pojo;
+
+public class Actor {
+	private String nombre;
+	private int nacimiento;
+	
+	public Actor(String nombre, int nacimiento) {
+		this.nombre = nombre;
+		this.nacimiento = nacimiento;
+	}
+}
